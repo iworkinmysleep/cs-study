104e911d9f99341fe05a85b049376db3e444b00f
Jason Hinton <jason_hinton@att.net>
2021-06-07 22:43:24 -0400

    initial

---
+++ scratchPad.js
@@ -0,0 +1,25 @@
+function charCount(str) {
+	const result = {};
+	for (let char of str) {
+		if (isAlphaNumeric(char)) {
+			char = char.toLowerCase();
+			result[char] = ++result[char] || 1;
+		}
+	}
+	return result;
+}
+
+function isAlphaNumeric(char) {
+	let code = char.charCodeAt(0);
+	if (
+		!(code > 47 && code < 58) && //numeric (0-9)
+		!(code > 64 && code < 91) && //upper alpha (A-Z)
+		!(code > 96 && code < 123)
+	) {
+		//lower alpha (a-z)
+		return false;
+	}
+	return true;
+}
+
+
